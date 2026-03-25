2c9636f6715f770e77b6f925661534032509a12f
tiesen243 <ttien56906@gmail.com>
2026-03-25 19:12:46 +0700

    Remove unused timer_init_1s function from timer_ctrl.c and timer_ctrl.h
    The timer_init_1s function was removed from both the implementation and header files as it was not being utilized in the codebase. This cleanup helps streamline the timer control functionality.

--- fpga/software/fpga/include/timer_ctrl.c
+++ fpga/software/fpga/include/timer_ctrl.c
@@ -19,22 +19,6 @@ void timer_init(unsigned int ms)
                                          ALTERA_AVALON_TIMER_CONTROL_START_MSK);
 }
 
-void timer_init_1s(void)
-{
-    unsigned int period = 50000000 - 1;
-
-    IOWR_ALTERA_AVALON_TIMER_CONTROL(TIMER_BASE,
-                                     ALTERA_AVALON_TIMER_CONTROL_STOP_MSK);
-
-    IOWR_ALTERA_AVALON_TIMER_PERIODL(TIMER_BASE, period);
-    IOWR_ALTERA_AVALON_TIMER_PERIODH(TIMER_BASE, period >> 16);
-
-    IOWR_ALTERA_AVALON_TIMER_CONTROL(TIMER_BASE,
-                                     ALTERA_AVALON_TIMER_CONTROL_CONT_MSK |
-                                         ALTERA_AVALON_TIMER_CONTROL_ITO_MSK |
-                                         ALTERA_AVALON_TIMER_CONTROL_START_MSK);
-}
-
 void timer_clear_timeout(void)
 {
     IOWR_ALTERA_AVALON_TIMER_STATUS(TIMER_BASE,
--- fpga/software/fpga/include/timer_ctrl.h
+++ fpga/software/fpga/include/timer_ctrl.h
@@ -1,7 +1,6 @@
 #ifndef TIMER_CTRL_H
 #define TIMER_CTRL_H
 
-void timer_init_1s(void);
 void timer_init(unsigned int ms);
 void timer_clear_timeout(void);
 
